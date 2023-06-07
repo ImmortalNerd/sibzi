c07696fe760b5694a57fa515ad0aed88cd36b6fc
Sepide <sepisoltan@gmail.com>
2023-06-07 18:06:17 +0330

    creating logo

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
   const theme = useTheme();
   return (
     <div className='container mt-2'>
-      <div className='flex'>
-        <div className={`${styles.circle}`}>
-          <div><Image src='/Group 16.png' alt='' width={54.46} height={102}/></div>
+      <div className='flex justify-center'>
+        <div className={`flex justify-center mt-6 p-3 ${styles.circle}`}>
+          <Image src='/Group 16.png' alt='' width={54.46} height={102}/>
         </div>
       </div>
       
