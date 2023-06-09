989fc0a636c047465a654195634a79c7808cf763
Sepide <sepisoltan@gmail.com>
2023-06-09 17:35:03 +0330

    changing sign up css

--- components/SignUp.js
+++ components/SignUp.js
@@ -7,17 +7,11 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 
 export default function SignUp() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card >
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+          <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 'bold'}}>
+            Hi there!
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,26 @@ import Image from 'next/image'
 import { Button } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import styles from './home.module.css'
+import SignUp from '@/components/SignUp';
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const theme = useTheme();
   return (
+    <>
     <div className='container mt-2'>
       <div className='flex justify-center'>
-        <div className={`flex justify-center mt-6 p-3 ${styles.circle}`}>
+        <div className={`icon flex justify-center mt-16 p-3 ${styles.circle}`}>
           <Image src='/Group 16.png' alt='' width={54.46} height={102}/>
         </div>
       </div>
-      
     </div>
+    <div className='container'>
+      <div className='flex mt-16 justify-center'>
+          <SignUp />
+      </div>
+    </div> 
+    </>
   )
 }
