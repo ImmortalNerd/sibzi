268430002008019b55453e5932c975bafe978ce0
Sepide <sepisoltan@gmail.com>
2023-06-10 16:00:12 +0330

    creating sign up component finished

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,26 +1,62 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import styles from './signup.module.css'
 import { Button, CardActionArea, CardActions, TextField } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Link from 'next/link';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import AppleIcon from '@mui/icons-material/Apple';
+
+const CssTextField = styled(TextField)({
+  '& .MuiInputBase-root': {
+      border: 'none',
+      borderRadius: '12px',
+  },
+  '& .MuiFilledInput-root::before': {
+      border: 'none',
+  },
+  '& .MuiFilledInput-root::after': {
+      border: 'none',
+  },
+});
+
 
 export default function SignUp() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
-    <Card sx={{ width: 1 }}>
-      <CardActionArea>
-        <CardContent>
+    <form className={`${styles.signup} p-8 mb-0`}>
           <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 'bold'}}>
             Hi there!
-          </Typography>
-           {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
+      </Typography>
+        <Stack className='gap-3 justify-center'>
+          <CssTextField id="filled-basic" type="number" label="Phone Number" variant="filled" InputProps={{
+              disableUnderline: true,
+            }}/>
+          <CssTextField id="filled-basic" label="Name" variant="filled" InputProps={{
+              disableUnderline: true,
+            }}/>
+          <CssTextField id="filled-basic" type="password" label="Password" variant="filled" InputProps={{
+              disableUnderline: true,
+            }}/>
+          <Button size="small" color="secondary">
+            <Link href="#">Forgot Password?</Link> 
+          </Button>
+          <Button className='signup-btn' variant="contained" color='primary'>Sign Up</Button>
+          <p className='text-center'>Or</p>
+          <Stack className='justify-center' direction="row" spacing={2}>
+            <Link href="#" className={styles.icons}><GoogleIcon /></Link>
+            <Link href="#" className={styles.icons}><FacebookOutlinedIcon /></Link>
+            <Link href="#" className={styles.icons}><AppleIcon /></Link>
+        </Stack>
+        <p className='text-center'>Already have an account?
+          <Button size="small" color="secondary">
+            <Link href="#" onClick={handleShowSignIn}>Sign In</Link> 
+          </Button>
+        </p>
+        </Stack>
+      
+    </form>
   );
 }
